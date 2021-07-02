9589df2651350fe80249b2ea2a6ef5bc34489eb8
Karmavir Joshi <karmavirj@live.com>
2021-07-03 01:51:54 +0530

    test if can get env vars

--- admin/index.php
+++ admin/index.php
@@ -1,23 +1,8 @@
 <?php
         ob_start();
         session_start();
-        if (isset($_SESSION['test'])){
-            echo 'persit';
-        }
-ini_set('session.save_handler', 'memcached');
-ini_set('session.save_path', getenv('MEMCACHIER_SERVERS'));
-if(version_compare(phpversion('memcached'), '3', '>=')) {
-    ini_set('memcached.sess_persistent', 1);
-    ini_set('memcached.sess_binary_protocol', 1);
-} else {
-    ini_set('session.save_path', 'PERSISTENT=myapp_session ' . ini_get('session.save_path'));
-    ini_set('memcached.sess_binary', 1);
-}
-ini_set('memcached.sess_sasl_username', getenv('MEMCACHIER_USERNAME'));
-ini_set('memcached.sess_sasl_password', getenv('MEMCACHIER_PASSWORD'));
         require 'connect.php';
-        $_SESSION['test']=1;
-        echo $_SESSION['test'];
+        echo getenv('MEMCACHIER_SERVERS');
 if(isset($_SESSION['u_id']))
 {
     header('location:dashboard.php');
