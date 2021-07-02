d8185d5e5e66f66c299d16c206a5127ae779723d
Karmavir Joshi <karmavirj@live.com>
2021-07-03 00:27:47 +0530

    db connect changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '95518a4db835008b63e995018e4a499f16614c5b',
+        'name' => 'karmavir/merry-fish',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'karmavir/merry-fish' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '95518a4db835008b63e995018e4a499f16614c5b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
