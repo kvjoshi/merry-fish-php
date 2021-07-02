96d95eba2a66537931f63089f7f5ea24ac0ad498
Karmavir Joshi <karmavirj@live.com>
2021-07-03 01:27:46 +0530

    session changes

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 // any ini_set() for session configuration goes here when not using .user.ini
 
 session_start();
+$_SESSION['count']=3;
 if (!isset($_SESSION['count'])) {
-    $_SESSION['count'] = 0;
+    $_SESSION['count'] = 2;
 }
-$_SESSION['count']++;
+
 
 echo "Hello #" . $_SESSION['count'];
