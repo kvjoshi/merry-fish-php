3fb6daa82c5fd414beb6c9f68e5d2d9cf7d6e6d2
Karmavir Joshi <karmavirj@live.com>
2021-07-16 17:47:30 +0530

    analytics added

--- web/admin/past_ord.php
+++ web/admin/past_ord.php
@@ -36,6 +36,15 @@ $ords_query=mysqli_query($con,$ords_sql);
     <!--Common Plugins CSS -->
     <link href="css/plugins/plugins.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-P4L8S9EXV2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-P4L8S9EXV2');
+</script>
     <!--fonts-->
     <link href="lib/line-icons/line-icons.css" rel="stylesheet">
 
--- web/index.php
+++ web/index.php
@@ -18,6 +18,15 @@
     <!--Common Plugins CSS -->
     <link href="css/plugins/plugins.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-P4L8S9EXV2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-P4L8S9EXV2');
+</script>
     <!--fonts-->
     <link href="lib/line-icons/line-icons.css" rel="stylesheet">
     <link href="lib/font-awesome/css/fontawesome-all.min.css" rel="stylesheet">
