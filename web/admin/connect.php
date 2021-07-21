b11d5271e4b4bff38aadcad8959903ba3503c3ed
Karmavir Joshi <karmavirj@live.com>
2021-07-21 17:19:08 +0530

    Update connect.php

--- web/admin/connect.php
+++ web/admin/connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "merry-fish";
+$servername = "u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "tdmw9818germj6lk";
+$password = "oe54liai5o5pd6bs";
+$dbname = "y66lzsemeg5bpszn";
 
 // Create connection
 $con = new mysqli($servername, $username, $password, $dbname);
